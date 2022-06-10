03fd60701874e968cdfc68f0102213883a27f72e
PavanBandichode <pavan.bandichode@gmail.com>
2022-06-10 16:24:46 +0530

    shopping cart

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="footer">
+          <p>
+            made by... ❤️
+            <span>Pavan B.</span>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+export default Footer;
